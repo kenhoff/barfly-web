228a66d6f90069b126513b3db8368af0c0d6acb3
Ken Hoff <ken@hoff.tech>
2015-12-18 19:25:48 -0700

    react working with jsx compiler

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("asdfasdfasdf");
+
+var BarflyMain = React.createClass({
+	render: function () {
+		return (
+			<div>
+				hullo world
+			</div>
+		);
+	}
+})
+
+ReactDOM.render(
+	<BarflyMain />,
+	document.getElementById('content')
+)
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 var app = require('express')();
+var harp = require('harp');
 port = process.env.PORT || 1175
 
-app.get("/", function (req, res) {
-	res.sendfile("index.html")
-})
-
+app.use(harp.mount(__dirname))
 
 app.listen(port, function () {
 	console.log("Listening on", port);
