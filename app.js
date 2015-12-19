48c63203f920e87091f74c652a5e7f5bbb8fcf4f
Ken Hoff <ken@hoff.tech>
2015-12-18 19:54:34 -0700

    first whack at auth0

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
-console.log("asdfasdfasdf");
-
 var BarflyMain = React.createClass({
 	render: function () {
-		return (
-			<div>
-				hullo world
-			</div>
-		);
+		if (this.state.idToken) {
+			return (
+				<div>
+					Welcome!
+				</div>
+			);
+		}
+		else {
+			return (
+				<div>
+				<a onClick={this.showLock}>Show Lock</a>
+				</div>
+			);
+		}
+	},
+	componentWillMount: function() {
+		this.lock = new Auth0Lock('JeIT5hdK0PXWuMVE1GSYbDT4Uw2HQpKx', 'barfly.auth0.com');
+		this.setState({idToken: this.getIdToken()})
+	},
+	getIdToken: function () {
+		var idToken = localStorage.getItem("access_jwt")
+		var authHash = this.lock.parseHash(window.location.hash)
+		if (!idToken && authHash) {
+			if (authHash.id_token) {
+				idToken = authHash.id_token
+				// this is correct - we want to store and use the full JWT, not just the "access_token" in the authHash
+				localStorage.setItem("access_jwt", authHash.id_token)
+				localStorage.setItem("refresh_token", authHash.refresh_token)
+				// this is pretty hacky - get rid of the hash when the page gets redirected.
+				window.location.hash = ""
+			}
+			if (authHash.error){
+				console.log("Error signing in with authHash:", authHash);
+				return null
+			}
+		}
+		return idToken
+	},
+	showLock: function () {
+		this.lock.show({ authParams: { scope: "openid offline_access"}})
 	}
 })
 
