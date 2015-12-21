7a81d7aebc8d1e4aeaaf6f9503a57cf12a6899ed
Ken Hoff <ken@hoff.tech>
2015-12-20 19:01:23 -0700

    properly working with browserify now

--- js/barflyApp.js
+++ js/barflyApp.js
@@ -1,3 +1,5 @@
+var React = require('react');
+
 var BarflyApp = React.createClass({
 	getInitialState: function () {
 		return {
--- js/barflyMain.js
+++ js/barflyMain.js
@@ -1,3 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
 var BarflyApp = require('./barflyApp');
 
 var BarflyMain = React.createClass({
