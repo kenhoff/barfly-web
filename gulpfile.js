5aa5e1754da012f8906d7f705343d98b2bc00aaf
Ken Hoff <ken@hoff.tech>
2016-01-15 15:02:49 -0700

    starting to use dotenv

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV != "production") {
+	require('dotenv').load()
+}
+
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
