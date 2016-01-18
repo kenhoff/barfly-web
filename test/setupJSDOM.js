f18f0ddb803e76256faefa3b3e8035586be1ae7d
Ken Hoff <ken@hoff.tech>
2016-01-18 15:06:51 -0700

    added jsdom

---
+++ test/setupJSDOM.js
@@ -0,0 +1,22 @@
+console.log("setting up jsdom...");
+var jsdom = require('jsdom');
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+
+// grabbed from http://jaketrent.com/post/testing-react-with-jsdom/
+
+// take all properties of the window object and also attach it to the
+// mocha global object
+propagateToGlobal(global.window)
+
+// from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
+function propagateToGlobal(window) {
+	for (key in window) {
+		if (!window.hasOwnProperty(key)) continue
+		if (key in global) continue
+
+		global[key] = window[key]
+	}
+}
+
+console.log("done setting up jsdom");
