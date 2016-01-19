963e9c319b79b0efdbaff81eb4eed49aceb1b0ac
Ken Hoff <ken@hoff.tech>
2016-01-19 15:25:25 -0700

    testing actually starting to work

--- test/NewBarModal.jsx
+++ test/NewBarModal.jsx
@@ -2,25 +2,52 @@ var assert = require('assert');
 
 var NewBarModal = require('../js/NewBarModal.jsx');
 var React = require('react');
+var ReactDOM = require('react-dom');
 var ReactTestUtils = require('react-addons-test-utils');
 var Modal = require('react-bootstrap').Modal;
 var Input = require('react-bootstrap').Input;
 
+// disclaimer - this is all bullshit. check https://github.com/react-bootstrap/react-bootstrap/blob/master/test/ModalSpec.js to see how to actually test these modals
+
 describe("NewBarModal", function() {
+
+	beforeEach(() => {
+		mountPoint = document.createElement('div');
+		document.body.appendChild(mountPoint);
+	});
+
+	afterEach(() => {
+		ReactDOM.unmountComponentAtNode(mountPoint);
+		document.body.removeChild(mountPoint);
+	});
+
 	it("renders a Modal", function(done) {
-		renderedComponent = ReactTestUtils.renderIntoDocument(< NewBarModal />)
-		assert(ReactTestUtils.findRenderedComponentWithType(renderedComponent, Modal))
-		modalComponent = ReactTestUtils.findRenderedComponentWithType(renderedComponent, Modal)
-		console.log(ReactTestUtils.findRenderedComponentWithType(modalComponent, Modal.Header));
+		renderedComponent = ReactDOM.render(< NewBarModal showModal = {
+			true
+		} />, mountPoint)
+		assert(ReactTestUtils.isCompositeComponentWithType(renderedComponent.refs.NewBarModal, Modal))
 		done()
 	})
-	it("has two Input fields", function() {
-		renderedComponent = ReactTestUtils.renderIntoDocument(< NewBarModal />)
-		// console.log(ReactTestUtils.scryRenderedComponentsWithType(renderedComponent, Input).length)
-		assert(ReactTestUtils.scryRenderedComponentsWithType(renderedComponent, Input).length == 2)
+
+	it("has three buttons?!?!?!?!", function(done) {
+		renderedComponent = ReactTestUtils.renderIntoDocument(< NewBarModal showModal = {
+			true
+		} />)
+		buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(renderedComponent.refs.NewBarModal._modal, "button")
+		assert(buttons.length == 3)
 		done()
 	})
-	it("zip code Input has the value '12345' when '12345' is entered")
+	it("has two Input fields", function(done) {
+		renderedComponent = ReactTestUtils.renderIntoDocument(< NewBarModal showModal = {
+			true
+		} />)
+		assert(ReactTestUtils.scryRenderedDOMComponentsWithTag(renderedComponent.refs.NewBarModal._modal, "input").length == 2)
+		done()
+	})
+	it("zip code Input has the value '12345' when '12345' is entered", function (done) {
+		assert(resultValue == "12345")
+		done()
+	})
 	it("zip code Input has the value '67890' when '678900' is entered")
 	it("zip code Input has the value '' when 'asdfasdfasdf' is entered")
 	it("zip code Input has the value '12345' when 'a1s2d3f4a5sdf' is entered")
