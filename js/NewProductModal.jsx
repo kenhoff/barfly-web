29fe3159dd164498f0424c5d9a3a5dafd8888ff7
Ken Hoff <ken@hoff.tech>
2016-01-21 16:31:03 -0700

    NewProductModal validation checking and tests

--- js/NewProductModal.jsx
+++ js/NewProductModal.jsx
@@ -1,34 +1,39 @@
 var React = require('react');
+var $ = require('jquery');
 
 var Modal = require('react-bootstrap').Modal;
 var Input = require('react-bootstrap').Input;
 
-
 var NewProductModal = React.createClass({
 	getInitialState: function() {
-		return ({showNewSizeInput: false, sizes: []})
+		return ({showNewSizeInput: false, sizes: [], buttonEnabled: false})
 	},
 	componentDidMount: function() {
 		// bullshit that we can't initialize the ref to productSizeInput here, but whatever. We can work around it somehow
 		this.updateSizes()
+		// this.updateFormIsValid()
 	},
 	updateSizes: function() {
 		$.ajax({
 			url: window.API_URL + "/sizes",
 			method: "GET",
 			success: function(sizes) {
-				this.setState({sizes: sizes})
+				if (sizes.length == 0) {
+					this.setState({sizes: sizes, showNewSizeInput: true})
+				} else {
+					this.setState({sizes: sizes, showNewSizeInput: false})
+				}
 			}.bind(this)
 		})
 	},
 	render: function() {
 		return (
-			<Modal show={this.props.showModal} onHide={this.props.onHide}>
+			<Modal show={this.props.showModal} onHide={this.props.onHide} ref="NewProductModal">
 				<Modal.Header closeButton>
 					<Modal.Title>Let's add a new product to Barfly.</Modal.Title>
 				</Modal.Header>
 				<Modal.Body>
-					<Input type="text" label="What's the name of your product?" placeholder="Bob's Genuine Beer" ref="productNameInput"/>
+					<Input type="text" label="What's the name of your product?" placeholder="Bob's Genuine Beer" ref="productNameInput" onChange={this.updateFormIsValid}/>
 					<Input type="select" label="What size does your product come in?" ref="productSizeInput" onChange={this.selectSize}>
 						{this.state.sizes.map(function(size) {
 							return (<SizeOption key={size} sizeID={size}/>)
@@ -37,35 +42,54 @@ var NewProductModal = React.createClass({
 					</Input>
 					<Input className={this.state.showNewSizeInput
 						? "show"
-						: "hidden"} type="text" placeholder="750ml (Case of 6)" ref="productNewSizeInput"/>
+						: "hidden"} type="text" placeholder="750ml" ref="productNewSizeInput" onChange={this.updateFormIsValid}/>
 				</Modal.Body>
 				<Modal.Footer>
 					<button className="btn btn-default" onClick={this.props.onHide}>Cancel</button>
-					<button className="btn btn-primary" onClick={this.submitProduct}>Create</button>
+					<button className={"btn btn-primary " + (this.state.buttonEnabled
+						? ""
+						: "disabled")} onClick={this.submitProduct}>Create</button>
 				</Modal.Footer>
 			</Modal>
-		);
+		)
+	},
+	updateFormIsValid: function() {
+		if (this.refs.productNameInput.getValue().trim() != "") {
+			if ((this.refs.productSizeInput.getValue() == "newSize") && (this.refs.productNewSizeInput.getValue().trim() != "")) {
+				this.setState({buttonEnabled: true})
+			} else if (parseInt(this.refs.productSizeInput.getValue()) > 0) {
+				this.setState({buttonEnabled: true})
+			} else {
+				this.setState({buttonEnabled: false})
+			}
+		} else {
+			this.setState({buttonEnabled: false})
+		}
 	},
 	submitProduct: function() {
 		// if the user has specified a new size, POST /sizes first, then POST /products with the new ID.
 		// if the user didn't specify a new size, then just POST /products with the given ID.
-
-		if (this.refs.productSizeInput.getValue() == "newSize") {
-			this.sendNewSize(this.refs.productNewSizeInput.getValue(), function(newSizeID) {
-				this.sendNewProduct(this.refs.productNameInput.getValue(), newSizeID, function() {
+		productName = this.refs.productNameInput.getValue().trim()
+		if (this.state.buttonEnabled) {
+			if (this.refs.productSizeInput.getValue() == "newSize") {
+				this.sendNewSize(this.refs.productNewSizeInput.getValue(), function(newSizeID) {
+					this.sendNewProduct(productName, newSizeID, function() {
+						this.updateSizes()
+						this.props.newProductCreated()
+						this.props.onHide()
+						this.setState({showNewSizeInput: false})
+					}.bind(this))
+				}.bind(this))
+			} else {
+				this.sendNewProduct(productName, this.refs.productSizeInput.getValue(), function() {
 					this.updateSizes()
 					this.props.newProductCreated()
 					this.props.onHide()
 					this.setState({showNewSizeInput: false})
 				}.bind(this))
-			}.bind(this))
+			}
 		} else {
-			this.sendNewProduct(this.refs.productNameInput.getValue(), this.refs.productSizeInput.getValue(), function() {
-				this.updateSizes()
-				this.props.newProductCreated()
-				this.props.onHide()
-				this.setState({showNewSizeInput: false})
-			}.bind(this))
+			// do a toast or something
 		}
 
 	},
@@ -75,6 +99,7 @@ var NewProductModal = React.createClass({
 		} else {
 			this.setState({showNewSizeInput: false})
 		}
+		this.updateFormIsValid()
 	},
 	sendNewSize: function(sizeName, cb) {
 		$.ajax({
@@ -102,7 +127,7 @@ var NewProductModal = React.createClass({
 			method: "POST",
 			data: {
 				productName: name,
-				productSize: sizeID
+				productSize: parseInt(sizeID)
 			},
 			success: function(data) {
 				if (cb) {
@@ -111,7 +136,6 @@ var NewProductModal = React.createClass({
 			}.bind(this)
 		})
 	}
-	// don't forget to get product sizes, populate modal
 })
 
 var SizeOption = React.createClass({
