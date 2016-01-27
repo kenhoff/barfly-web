eba2c037419f3573a85b20c3d7cd0457075dff2c
Ken Hoff <ken@hoff.tech>
2016-01-26 20:13:35 -0700

    list of inputs, one for each size

---
+++ js/QuantityInputWithSizeList.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Input = require('react-bootstrap').Input;
+var Button = require('react-bootstrap').Button;
+
+var QuantityInputWithSize = require('./QuantityInputWithSize.jsx');
+
+// passed in a list of sizes, and quantities
+
+var QuantityInputWithSizeList = React.createClass({
+	render: function() {
+		if (this.props.productSizes && (this.props.productSizes.length != 0)) {
+			return (
+				<div>
+					{this.props.productSizes.map(function(sizeID) {
+						return (<QuantityInputWithSize key={sizeID} sizeID={sizeID} quantity={this.getQuantityForSizeID(sizeID)} changeQuantity={this.props.changeQuantity.bind(this, sizeID)}/>)
+					}.bind(this))}
+				</div>
+			)
+		} else {
+			return (
+				<div>no product sizes found, add a new product size</div>
+			)
+		}
+	},
+	getQuantityForSizeID: function(sizeID) {
+		for (quantity of this.props.quantities) {
+			if (quantity.productSizeID == sizeID) {
+				return quantity.productQuantity
+			}
+		}
+	}
+});
+
+module.exports = QuantityInputWithSizeList;
