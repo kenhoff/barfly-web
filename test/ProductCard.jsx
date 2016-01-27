a104ca6165d1f7ad6a13e0ea66860f8923b68120
Ken Hoff <ken@hoff.tech>
2016-01-26 20:11:17 -0700

    see previous commit

--- test/ProductCard.jsx
+++ test/ProductCard.jsx
@@ -6,12 +6,34 @@ var ReactTestUtils = require('react-addons-test-utils');
 
 var ProductCard = require('../js/ProductCard.jsx');
 
+renderProductCard = function() {
+	ReactTestUtils.renderIntoDocument(< ProductCard />)
+}
+
 describe("ProductCard", function() {
-	shallowRenderer = ReactTestUtils.createRenderer()
-	shallowRenderer.render(<ProductCard productID = {1} barID = "2" />)
-	result = shallowRenderer.getRenderOutput()
-	it("is actually a react element", function (done) {
-		assert(ReactTestUtils.isElement(result));
-		done()
+
+	beforeEach(function() {
+		renderedProductCard = renderProductCard()
+	})
+
+	it("doesn't render anything if a productID isn't provided", function(done) {})
+	it("doesn't render anything if a barID isn't provided")
+	describe("if everything is provided", function() {
+		it("renders a panel")
+		it("renders the correct product name")
+	})
+	describe("distributor/rep availability", function() {
+		describe("if there's no distributor for this product in this zip code", function() {
+			it("displays 'no distributor found'")
+		})
+		describe("if there's no rep found for this bar with this distributor", function() {
+			it("if there's no rep found for this bar with this distributor, displays 'no rep found'")
+		})
+		describe("if there is a distributor for this product in this zide code", function() {
+			it("displays the distributor name")
+		})
+		describe("if there is a rep found for this bar with this distributor", function() {
+			it("displays the rep name")
+		})
 	})
 })
