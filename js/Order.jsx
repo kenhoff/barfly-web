280dc660d18a0ab563ba71635f717d7a7720948e
Ken Hoff <ken@hoff.tech>
2016-01-26 20:14:22 -0700

    Order now passes in the sizes and quantities to the product card, and seems to handle the updates to the order just as before

--- js/Order.jsx
+++ js/Order.jsx
@@ -18,9 +18,8 @@ var Order = React.createClass({
 	},
 	getInitialState: function() {
 		// allProducts is a list of all products that we carry, with each product having a different size.
-		// orderProducts is a list of all products currently in the order (quantity > 0)
-		// listProduct will be several arrays, each with a list of the products in a particular list (like starred products)
-		return {allProducts: [], orderProducts: [], showNewProductModal: false, sent: true}
+		// productOrders is a list of all products currently in the order (quantity > 0)
+		return {allProducts: [], productOrders: [], showNewProductModal: false, sent: true}
 	},
 	componentWillUnmount: function() {
 		clearTimeout(this.timeout)
@@ -29,8 +28,8 @@ var Order = React.createClass({
 		return (
 			<div>
 				<h1>Order #{this.props.params.orderID}</h1>
-				{this.state.allProducts.map(function(product) {
-					return (<ProductCard key={product.productID.toString() + product.productSizeID.toString()} productID={product.productID} productSizeID={product.productSizeID} productQuantity={this.getProductQuantity(product.productID, product.productSizeID)} changeQuantity={this.handleQuantityChange} barID={this.props.bar} reresolveOrder={this.reresolveOrder} disabled={this.state.sent}/>)
+				{this.state.allProducts.map(function(productID) {
+					return (<ProductCard key={productID} productID={productID} barID={this.props.bar} quantities={this.getQuantitiesForProduct(productID)} changeQuantity={this.handleQuantityChange} reresolveOrder={this.reresolveOrder} disabled={this.state.sent}/>)
 				}.bind(this))}
 				<p>Can't find what you're looking for?&nbsp;
 					<a onClick={this.showNewProductModal}>Create a new product</a>
@@ -39,6 +38,18 @@ var Order = React.createClass({
 				<OrderNavBottom disabled={this.state.sent} sendOrder={this.sendOrder}/>
 			</div>
 		)
+	},
+
+	// i think these might be the same function
+
+	getQuantitiesForProduct: function(productID) {
+		productQuantities = []
+		for (productOrder of this.state.productOrders) {
+			if (productOrder.productID == productID) {
+				productQuantities.push({productQuantity: productOrder.productQuantity, productSizeID: productOrder.productSizeID})
+			}
+		}
+		return productQuantities
 	},
 
 	sendOrder: function() {
@@ -61,47 +72,42 @@ var Order = React.createClass({
 		this.setState({showNewProductModal: false})
 	},
 
-	getProductQuantity: function(productID, productSizeID) {
-		for (product of this.state.orderProducts) {
-			if ((product.productID == productID) && (product.productSizeID == productSizeID)) {
-				return product.productQuantity
-			}
-		}
-		return null
-	},
-
 	// yay clusterfuck!
 	handleQuantityChange: function(productID, productSizeID, productQuantity) {
+
+		console.log("productID", productID);
+		console.log("productSizeID", productSizeID);
+		console.log("productQuantity", productQuantity);
 
 		// updateTimeout handles the order patching
 		this.updateTimeout()
 		// change existing state to reflect new quantity change
 
-		// if combination of productID and productSizeID exist in orderProducts,
+		// if combination of productID and productSizeID exist in productOrders,
 		this.setState(function(prevState, currentProps) {
-			for (var i = 0; i < prevState.orderProducts.length; i++) {
-				if (((prevState.orderProducts[i].productID == productID) && (prevState.orderProducts[i].productSizeID == productSizeID))) {
-					// if productQuantity == 0, then remove from orderProducts.splice(i, 1)
+			for (var i = 0; i < prevState.productOrders.length; i++) {
+				if (((prevState.productOrders[i].productID == productID) && (prevState.productOrders[i].productSizeID == productSizeID))) {
+					// if productQuantity == 0, then remove from productOrders.splice(i, 1)
 					if (isNaN(productQuantity) || productQuantity <= 0) {
-						prevState.orderProducts.splice(i, 1)
+						prevState.productOrders.splice(i, 1)
 					} else {
 						// update that particular combination of productID and productSizeID with productQuantity
-						prevState.orderProducts[i] = {
+						prevState.productOrders[i] = {
 							productID: productID,
 							productSizeID: productSizeID,
 							productQuantity: productQuantity
 						}
 					}
-					return ({orderProducts: prevState.orderProducts})
+					return ({productOrders: prevState.productOrders})
 				}
 			}
-			// else, insert that particular combination of productID, productSizeID and productQuantity into orderProducts
+			// else, insert that particular combination of productID, productSizeID and productQuantity into productOrders
 			if (!isNaN(productQuantity)) {
-				newOrderProducts = this.state.orderProducts
+				newOrderProducts = this.state.productOrders
 				newOrderProducts.push({productID: productID, productSizeID: productSizeID, productQuantity: productQuantity})
 				// next, send a PATCH to /orders/:orderID with new order state
 				// this.patchOrder(newOrderProducts)
-				return ({orderProducts: newOrderProducts})
+				return ({productOrders: newOrderProducts})
 			}
 		})
 
@@ -109,7 +115,7 @@ var Order = React.createClass({
 		// keep in mind that we're sending entire state on change, so if anything needs to catch up it'll happen later
 	},
 	reresolveOrder: function() {
-		this.setState({allProducts: [], orderProducts: []})
+		this.setState({allProducts: [], productOrders: []})
 		this.getProducts()
 		this.getOrder()
 	},
@@ -122,18 +128,19 @@ var Order = React.createClass({
 			},
 			method: "GET",
 			success: function(data) {
+				console.log(data.productOrders);
 				// handle if sent isn't actually in the order yet
 				this.setState({
-					orderProducts: data.productOrders,
+					productOrders: data.productOrders,
 					sent: (data.sent || false)
 				})
 			}.bind(this)
 		})
 	},
 
-	patchOrder: function(orderProducts) {
+	patchOrder: function(productOrders) {
 		data = {
-			orders: this.state.orderProducts
+			orders: this.state.productOrders
 		},
 		$.ajax({
 			url: window.API_URL + "/bars/" + this.props.bar + "/orders/" + this.props.params.orderID,
@@ -152,17 +159,9 @@ var Order = React.createClass({
 			url: window.API_URL + "/products",
 			// (no auth needed)
 			method: "GET",
-			success: function(productsWithSizes) {
-				async.map(productsWithSizes, this.getSizesForProduct, function(err, unflattenedProducts) {
-					if (unflattenedProducts.length == 0) {
-						this.setState({allProducts: []})
-					} else {
-						flattenedProducts = unflattenedProducts.reduce(function(a, b) {
-							return a.concat(b)
-						})
-						this.setState({allProducts: flattenedProducts})
-					}
-				}.bind(this))
+			success: function(products) {
+				console.log(products);
+				this.setState({allProducts: products})
 			}.bind(this)
 		})
 	},
