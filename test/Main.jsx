17df0fe4c591ec51bae0ccba626d3b774c7e0f4e
Ken Hoff <ken@hoff.tech>
2016-02-12 12:12:08 -0700

    reordering tests

--- test/Main.jsx
+++ test/Main.jsx
@@ -1,13 +1,13 @@
 // failing to uncomment the other tests is a crime punishable by death
 
 // hold off on this one til we get everything fixed
-// require("./ProductCard.jsx")
 
-require("./OrderNavBottom.jsx")
-require('./NewBarModal.jsx');
-require('./NewProductModal.jsx');
 require('./AddDistributorModal.jsx');
 require('./AddRepModal.jsx');
-require('./QuantityInputWithSize.jsx')
-require('./Orders.jsx');
+require('./NewBarModal.jsx');
+require('./NewProductModal.jsx');
 require('./Order.jsx');
+require("./OrderNavBottom.jsx")
+require('./Orders.jsx');
+require("./ProductCard.jsx")
+require('./QuantityInputWithSize.jsx')
