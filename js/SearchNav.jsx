f0987699a82d5c6f428b4c52ed12e07bca02da85
Ken Hoff <ken@hoff.tech>
2016-02-21 18:33:14 -0700

    it's not centered, but it's got an input

--- js/SearchNav.jsx
+++ js/SearchNav.jsx
@@ -2,15 +2,16 @@ var React = require('react');
 var Navbar = require('react-bootstrap').Navbar;
 var Nav = require('react-bootstrap').Nav;
 var NavItem = require('react-bootstrap').NavItem;
+var Input = require('react-bootstrap').Input;
 
 var SearchNav = React.createClass({
 
 	render: function() {
 		return (
 			<Navbar fixedTop={this.props.fixedTop}>
-				<Nav>
-					<NavItem>This is where the search box would go</NavItem>
-				</Nav>
+				<Navbar.Form>
+					<Input type="text" placeholder="Search"/>
+				</Navbar.Form>
 			</Navbar>
 		);
 	}
