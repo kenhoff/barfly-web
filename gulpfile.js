4ed3ed90b70545c27818d6dca97a196f6010fbd1
Ken Hoff <ken@hoff.tech>
2016-02-23 11:42:06 -0700

    using browserify CLI in npm script to build, removing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,38 +7,6 @@ var rewireify = require('rewireify');
 
 var rimraf = require('rimraf');
 
-gulp.task('default', function() {
-	var watcher = gulp.watch(["test/*.jsx", "js/*.jsx"], ["build"])
-	gulp.start('build')
-})
-
-// let's be real, not a whole lot of this "vinyl-source-stream" stuff makes any sense to me.
-gulp.task('build', function() {
-	console.log("building...");
-	if (process.env.NODE_ENV != "production") {
-		require('dotenv').load()
-	}
-	rimraf("static/app.js", function() {
-		browserify({
-				entries: "./js/Main.jsx",
-				transform: [reactify, envify],
-			}).bundle()
-			.on('error', function(err) {
-				console.log(err.message);
-			})
-			.pipe(source("app.js"))
-			.pipe(gulp.dest("./static/"))
-			.on("end", function() {
-				console.log("all done!");
-			})
-	})
-})
-
-gulp.task('test', function() {
-	var watcher = gulp.watch(["test/*.jsx", "js/*.jsx"], ['build-test'])
-	gulp.start('build-test')
-})
-
 gulp.task('build-test', function() {
 	console.log("building test suite...");
 	if (process.env.NODE_ENV != "production") {
