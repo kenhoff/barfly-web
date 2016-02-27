2c17c27d45061627cdd13b09654eff02588ccfcd
Ken Hoff <ken@hoff.tech>
2016-02-27 14:00:55 -0700

    using jstz and moment to get user's timezone, and display the time that an order was sent, if it was sent

--- js/OrderCard.jsx
+++ js/OrderCard.jsx
@@ -1,19 +1,28 @@
 var React = require('react');
+var moment = require('moment-timezone');
+var jstz = require('jstimezonedetect');
+
+timezone = jstz.determine().name()
 
 var browserHistory = require('react-router').browserHistory;
 
 var OrderCard = React.createClass({
 	render: function() {
+		if (("sent" in this.props.order) && this.props.order.sent) {
+			displayTime = moment(this.props.order.sentAt).tz(timezone).format('llll')
+		} else {
+			displayTime = ""
+		}
 		return (
 			<div className="panel panel-default" onClick={this.navigateToOrder}>
 				<div className="panel-body">
-					Order #{this.props.order}
+					Order #{this.props.order.id} {displayTime}
 				</div>
 			</div>
 		);
 	},
 	navigateToOrder: function() {
-		browserHistory.push("/orders/" + this.props.order)
+		browserHistory.push("/orders/" + this.props.order.id)
 	}
 });
 
