b7c0df4f555478f3d01755f9add688b034017477
Ken Hoff <ken@hoff.tech>
2016-03-01 10:26:42 -0700

    :cookie: first whack at SentOrderContents

---
+++ js/SentOrderContents.jsx
@@ -0,0 +1,16 @@
+var React = require('react')
+var Grid = require('react-bootstrap').Grid
+
+var SentOrderContents = React.createClass({
+
+	render: function() {
+		return (
+			<Grid>
+				<h1>Sent Order Contents</h1>
+			</Grid>
+		)
+	}
+
+})
+
+module.exports = SentOrderContents
