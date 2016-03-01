21e3f556dbc22c5a31d2eba4ad3df023d21f229a
Ken Hoff <ken@hoff.tech>
2016-03-01 12:26:10 -0700

    rendering SentOrderMessagesDistributor component for each distributor

--- js/SentOrderMessages.jsx
+++ js/SentOrderMessages.jsx
@@ -1,13 +1,38 @@
 var React = require('react')
+var $ = require('jquery')
 
 var SentOrderMessages = React.createClass({
-
+	getInitialState: function() {
+		return {distributors: []}
+	},
 	render: function() {
 		return (
-			<h1>Messages</h1>
+			<div>
+				<h1>Messages</h1>
+				{this.state.distributors.map(function(distributor) {
+					return (<SentOrderMessagesDistributor id={distributor.id} distributor={distributor}/>)
+				})}
+			</div>
+		)
+	},
+	componentDidMount: function() {
+		// get list of distributors
+		$.ajax({
+			url: process.env.BURLOCK_API_URL + "/distributors",
+			method: "GET",
+			success: function(distributors) {
+				this.setState({distributors: distributors})
+			}.bind(this)
+		})
+	}
+})
+
+var SentOrderMessagesDistributor = React.createClass({
+	render: function() {
+		return (
+			<p>{this.props.distributor.distributorName}</p>
 		)
 	}
-
 })
 
 module.exports = SentOrderMessages
