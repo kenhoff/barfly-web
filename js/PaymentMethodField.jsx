1de7eda95e90af528268ca6dd19073d2d0704030
Ken Hoff <ken@hoff.tech>
2016-03-05 12:17:13 -0700

    getting paymentmethods

--- js/PaymentMethodField.jsx
+++ js/PaymentMethodField.jsx
@@ -1,4 +1,5 @@
 var React = require('react')
+var $ = require('jquery')
 
 var PaymentMethodField = React.createClass({
 
@@ -8,8 +9,18 @@ var PaymentMethodField = React.createClass({
 				<label>Payment Method</label>
 			</div>
 		)
-	}
+	},
+	componentDidMount: function() {
+		$.ajax({
+			url: process.env.BURLOCK_API_URL + "/paymentmethods",
+			method: "GET",
+			headers: {
+				"Authorization": "Bearer " + localStorage.getItem("access_jwt")
+			},
+			success: function() {}.bind(this)
+		})
 
+	}
 })
 
 module.exports = PaymentMethodField
