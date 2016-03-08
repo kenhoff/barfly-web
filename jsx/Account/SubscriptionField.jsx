d70a6040bf147338d69e17cd92d768d86a22f6b7
Ken Hoff <ken@hoff.tech>
2016-03-08 15:39:01 -0700

    cancel subscription

--- jsx/Account/SubscriptionField.jsx
+++ jsx/Account/SubscriptionField.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var $ = require('jquery');
 var Col = require('react-bootstrap').Col;
 var Button = require('react-bootstrap').Button;
+var Popover = require('react-bootstrap').Popover;
+var OverlayTrigger = require('react-bootstrap').OverlayTrigger;
 
 var SubscriptionField = React.createClass({
 	getInitialState: function() {
@@ -15,14 +17,27 @@ var SubscriptionField = React.createClass({
 			return (
 				<Col xs={12}>
 					<label>Subscription</label>
-					<Button onClick={this.postSubscription} bsStyle="primary">Activate subscription</Button>
+					<p>
+						<Button onClick={this.postSubscription} bsStyle="primary">Activate subscription</Button>
+					</p>
 				</Col>
 			);
 		} else {
+			var popover = (
+				<Popover title="This will cancel your subscription immediately." id="Cancel subscription">
+					<Button bsStyle="danger" onClick={this.deleteSubscription}>Cancel subscription</Button>
+				</Popover>
+			);
 			return (
 				<Col xs={12}>
-					<label>Subscription</label>
+					<label>Subscription</label >
 					<p>Standard Plan</p>
+					<OverlayTrigger trigger="click" rootClose placement="right" overlay={popover}>
+						<Button bsStyle="warning">
+							Cancel subscription
+						</Button>
+					</OverlayTrigger>
+
 				</Col>
 			);
 		}
@@ -50,7 +65,20 @@ var SubscriptionField = React.createClass({
 				this.componentDidMount();
 			}.bind(this)
 		});
+	},
+	deleteSubscription: function() {
+		$.ajax({
+			url: process.env.BURLOCK_API_URL + "/subscriptions",
+			method: "DELETE",
+			headers: {
+				"Authorization": "Bearer " + localStorage.getItem("access_jwt")
+			},
+			success: function() {
+				this.componentDidMount();
+			}.bind(this)
+		});
 	}
+
 });
 
 module.exports = SubscriptionField;
