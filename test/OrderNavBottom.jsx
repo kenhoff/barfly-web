ac77910da4c1160e0487ddab9583daee2bc61f7f
Ken Hoff <ken@hoff.tech>
2016-04-16 17:45:06 -0600

    commenting out tests, for now...

--- test/OrderNavBottom.jsx
+++ test/OrderNavBottom.jsx
@@ -1,36 +1,34 @@
+var React = require('react');
 var assert = require('chai').assert;
 var sinon = require('sinon');
 
-var React = require('react');
-var ReactDOM = require('react-dom');
 var ReactTestUtils = require('react-addons-test-utils');
-var ShallowTestUtils = require('react-shallow-testutils');
 
-var OrderNavBottom = require("../jsx/Order/OrderNavBottom.jsx")
+var OrderNavBottom = require("../jsx/Order/OrderNavBottom.jsx");
 
-renderOrderNavBottom = function(component) {
-	renderedOrderNavBottom = ReactTestUtils.renderIntoDocument(component)
-	return renderedOrderNavBottom
-}
+var renderOrderNavBottom = function(component) {
+	renderedOrderNavBottom = ReactTestUtils.renderIntoDocument(component);
+	return renderedOrderNavBottom;
+};
 
 describe("OrderNavBottom", function() {
 	it("renders by default, without any props", function(done) {
-		renderedOrderNavBottom = renderOrderNavBottom(< OrderNavBottom />)
-		nav = ReactTestUtils.findRenderedDOMComponentWithTag(renderedOrderNavBottom, "nav")
-		assert(nav)
-		done()
-	})
+		renderedOrderNavBottom = renderOrderNavBottom(< OrderNavBottom />);
+		nav = ReactTestUtils.findRenderedDOMComponentWithTag(renderedOrderNavBottom, "nav");
+		assert(nav);
+		done();
+	});
 	it("when called with disabled = false, renders", function(done) {
 		renderedOrderNavBottom = renderOrderNavBottom(< OrderNavBottom disabled = {
 			false
-		} />)
-		nav = ReactTestUtils.findRenderedDOMComponentWithTag(renderedOrderNavBottom, "nav")
-		assert(nav)
-		done()
-	})
+		} />);
+		nav = ReactTestUtils.findRenderedDOMComponentWithTag(renderedOrderNavBottom, "nav");
+		assert(nav);
+		done();
+	});
 	describe("if props.sending = true", function() {
 		beforeEach(function() {
-			sendOrderSpy = sinon.spy()
+			sendOrderSpy = sinon.spy();
 
 			renderedOrderNavBottom = renderOrderNavBottom(< OrderNavBottom disabled = {
 				false
@@ -40,29 +38,29 @@ describe("OrderNavBottom", function() {
 			}
 			sendOrder = {
 				sendOrderSpy
-			} />)
-			submitButton = ReactTestUtils.findRenderedDOMComponentWithTag(renderedOrderNavBottom, "button")
-		})
+			} />);
+			submitButton = ReactTestUtils.findRenderedDOMComponentWithTag(renderedOrderNavBottom, "button");
+		});
 		afterEach(function () {
 
-		})
+		});
 		it("button is active", function(done) {
 			assert(submitButton.className.includes('active'));
-			done()
-		})
+			done();
+		});
 		it("button text is 'Sending order...'", function(done) {
-			assert.equal(submitButton.innerHTML, "Sending order...")
-			done()
-		})
+			assert.equal(submitButton.innerHTML, "Sending order...");
+			done();
+		});
 		it("clicking on button does nothing", function(done) {
-			ReactTestUtils.Simulate.click(submitButton)
-			assert.equal(sendOrderSpy.callCount, 0)
-			done()
-		})
-	})
+			ReactTestUtils.Simulate.click(submitButton);
+			assert.equal(sendOrderSpy.callCount, 0);
+			done();
+		});
+	});
 	describe("if props.sending = false", function() {
 		beforeEach(function() {
-			sendOrderSpy = sinon.spy()
+			sendOrderSpy = sinon.spy();
 
 			renderedOrderNavBottom = renderOrderNavBottom(< OrderNavBottom disabled = {
 				false
@@ -72,21 +70,21 @@ describe("OrderNavBottom", function() {
 			}
 			sendOrder = {
 				sendOrderSpy
-			} />)
-			submitButton = ReactTestUtils.findRenderedDOMComponentWithTag(renderedOrderNavBottom, "button")
-		})
+			} />);
+			submitButton = ReactTestUtils.findRenderedDOMComponentWithTag(renderedOrderNavBottom, "button");
+		});
 		it("button is not active", function(done) {
 			assert(!submitButton.className.includes('active'));
-			done()
-		})
+			done();
+		});
 		it("button text is 'Send Order'", function(done) {
-			assert.equal(submitButton.innerHTML, "Send Order")
-			done()
-		})
+			assert.equal(submitButton.innerHTML, "Send Order");
+			done();
+		});
 		it('clicking on button sends order', function(done) {
-			ReactTestUtils.Simulate.click(submitButton)
-			assert.equal(sendOrderSpy.callCount, 1)
-			done()
-		})
-	})
-})
+			ReactTestUtils.Simulate.click(submitButton);
+			assert.equal(sendOrderSpy.callCount, 1);
+			done();
+		});
+	});
+});
--- test/QuantityInputWithSize.jsx
+++ test/QuantityInputWithSize.jsx
@@ -1,19 +1,20 @@
-var sinon = require("sinon")
+var React = require('react');
+var sinon = require("sinon");
 // var React = require("react")
-var ReactTestUtils = require("react-addons-test-utils")
-var ReactDOM = require('react-dom')
-var Input = require("react-bootstrap").Input
-var Button = require("react-bootstrap").Button
-var assert = require('assert')
+var ReactTestUtils = require("react-addons-test-utils");
+var ReactDOM = require('react-dom');
+var Input = require("react-bootstrap").Input;
+var Button = require("react-bootstrap").Button;
+var assert = require('assert');
 
-var $ = require('jquery')
+var $ = require('jquery');
 
-var QuantityInputWithSize = require('../jsx/Order/QuantityInputWithSize.jsx')
+var QuantityInputWithSize = require('../jsx/Order/QuantityInputWithSize.jsx');
 
 describe("QuantityInputWithSize", function() {
-	renderQuantityInputWithSize = function(quantity) {
+	var renderQuantityInputWithSize = function(quantity) {
 
-		changeQuantitySpy = sinon.spy()
+		changeQuantitySpy = sinon.spy();
 
 		renderedQuantityInputWithSize = ReactTestUtils.renderIntoDocument(< QuantityInputWithSize sizeID = {
 			10
@@ -23,167 +24,167 @@ describe("QuantityInputWithSize", function() {
 		}
 		changeQuantity = {
 			changeQuantitySpy
-		} />)
+		} />);
 
-		inputComponent = ReactTestUtils.findRenderedComponentWithType(renderedQuantityInputWithSize, Input)
-		buttons = ReactTestUtils.scryRenderedComponentsWithType(renderedQuantityInputWithSize, Button)
-		label = ReactTestUtils.findRenderedDOMComponentWithTag(renderedQuantityInputWithSize, "label")
+		inputComponent = ReactTestUtils.findRenderedComponentWithType(renderedQuantityInputWithSize, Input);
+		buttons = ReactTestUtils.scryRenderedComponentsWithType(renderedQuantityInputWithSize, Button);
+		label = ReactTestUtils.findRenderedDOMComponentWithTag(renderedQuantityInputWithSize, "label");
 
-		return renderedQuantityInputWithSize
-	}
+		return renderedQuantityInputWithSize;
+	};
 
 	beforeEach(function() {
-		ajaxStub = sinon.stub($, "ajax")
+		ajaxStub = sinon.stub($, "ajax");
 		ajaxStub.onFirstCall().yieldsTo("success", {
 			containerID: 1,
 			packagingID: 2
-		})
+		});
 		ajaxStub.onSecondCall().yieldsTo("success", {
 			id: 1,
 			containerName: "750ml"
-		})
+		});
 		ajaxStub.onThirdCall().yieldsTo("success", {
 			id: 2,
 			packagingName: "Pack of 6"
-		})
-		renderedQuantityInputWithSize = renderQuantityInputWithSize(100)
-	})
+		});
+		renderedQuantityInputWithSize = renderQuantityInputWithSize(100);
+	});
 
 	afterEach(function() {
-		$.ajax.restore()
-	})
+		$.ajax.restore();
+	});
 
 	it("renders an Input", function(done) {
-		assert(ReactTestUtils.isCompositeComponentWithType(inputComponent, Input))
-		done()
-	})
+		assert(ReactTestUtils.isCompositeComponentWithType(inputComponent, Input));
+		done();
+	});
 	it("renders an input with a star button and two button addons", function(done) {
-		assert.equal(buttons.length, 3)
-		done()
-	})
+		assert.equal(buttons.length, 3);
+		done();
+	});
 	it("has a label with the right size name", function(done) {
 		assert.equal(label.innerHTML, "750ml, Pack of 6");
-		done()
-	})
+		done();
+	});
 	describe("if a quantity isn't provided", function() {
 		beforeEach(function() {
-			renderedQuantityInputWithSize = renderQuantityInputWithSize()
-		})
+			renderedQuantityInputWithSize = renderQuantityInputWithSize();
+		});
 		it("the input value is empty", function(done) {
-			assert(!inputComponent.getValue())
-			done()
-		})
-	})
+			assert(!inputComponent.getValue());
+			done();
+		});
+	});
 	describe("if the quantity provided is 0", function() {
 		beforeEach(function() {
-			renderedQuantityInputWithSize = renderQuantityInputWithSize(0)
-		})
+			renderedQuantityInputWithSize = renderQuantityInputWithSize(0);
+		});
 		it("the input value is empty", function(done) {
-			assert(!inputComponent.getValue())
-			done()
-		})
+			assert(!inputComponent.getValue());
+			done();
+		});
 		it("clicking the '-' stepper doesn't do anything", function(done) {
-			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[1]))
-			assert.equal(changeQuantitySpy.callCount, 0)
-			done()
-		})
+			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[1]));
+			assert.equal(changeQuantitySpy.callCount, 0);
+			done();
+		});
 		it("clicking the '+' stepper calls changeQuantity with quantity: 1", function(done) {
-			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[2]))
-			assert(changeQuantitySpy.calledWith(1), "changeQuantity not called with correct args")
-			done()
-		})
-	})
+			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[2]));
+			assert(changeQuantitySpy.calledWith(1), "changeQuantity not called with correct args");
+			done();
+		});
+	});
 	describe("if the quantity provided is 1", function() {
 		beforeEach(function() {
-			renderedQuantityInputWithSize = renderQuantityInputWithSize(1)
-		})
+			renderedQuantityInputWithSize = renderQuantityInputWithSize(1);
+		});
 		it("the input value is 1", function(done) {
-			assert.equal(inputComponent.getValue(), 1)
-			done()
-		})
+			assert.equal(inputComponent.getValue(), 1);
+			done();
+		});
 		it("clicking the '-' stepper calls changeQuantity with 0", function(done) {
-			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[1]))
-			assert(changeQuantitySpy.calledWith(0), "changeQuantity not called with correct args")
-			done()
-		})
+			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[1]));
+			assert(changeQuantitySpy.calledWith(0), "changeQuantity not called with correct args");
+			done();
+		});
 		it("clicking the '+' stepper calls changeQuantity with quantity: 2", function(done) {
-			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[2]))
-			assert(changeQuantitySpy.calledWith(2), "changeQuantity not called with correct args")
-			done()
-		})
-	})
+			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[2]));
+			assert(changeQuantitySpy.calledWith(2), "changeQuantity not called with correct args");
+			done();
+		});
+	});
 	describe("if the quantity provided is 100", function() {
 		it("the provided value is in the input", function(done) {
-			assert.equal(inputComponent.getValue(), 100)
-			done()
-		})
+			assert.equal(inputComponent.getValue(), 100);
+			done();
+		});
 		it("clicking the '-' stepper calls changeQuantity with quantity: 99", function(done) {
-			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[1]))
-			assert(changeQuantitySpy.calledWith(99), "changeQuantity not called with correct args")
-			done()
-		})
+			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[1]));
+			assert(changeQuantitySpy.calledWith(99), "changeQuantity not called with correct args");
+			done();
+		});
 		it("clicking the '+' stepper calls changeQuantity with quantity: 101", function(done) {
-			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[2]))
-			assert(changeQuantitySpy.calledWith(101), "changeQuantity not called with correct args")
-			done()
-		})
+			ReactTestUtils.Simulate.click(ReactDOM.findDOMNode(buttons[2]));
+			assert(changeQuantitySpy.calledWith(101), "changeQuantity not called with correct args");
+			done();
+		});
 
-	})
+	});
 	describe("changes to the input component", function() {
 		it("putting in '1' results in changeQuantity getting called with '1'", function(done) {
-			inputComponent.getInputDOMNode().value = 1
-			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode())
-			assert(changeQuantitySpy.calledWith(1), "changeQuantity not called with correct args")
-			done()
-		})
+			inputComponent.getInputDOMNode().value = 1;
+			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode());
+			assert(changeQuantitySpy.calledWith(1), "changeQuantity not called with correct args");
+			done();
+		});
 		it("putting in '100' results in changeQuantity getting called with '100'", function(done) {
-			inputComponent.getInputDOMNode().value = 100
-			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode())
-			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args")
-			done()
-		})
+			inputComponent.getInputDOMNode().value = 100;
+			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode());
+			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args");
+			done();
+		});
 		it("putting in 'asdfasdfasdf100asdfasdfasdf' results in changeQuantity getting called with '100'", function(done) {
-			inputComponent.getInputDOMNode().value = "asdfasdfasdf100asdfasdfasdf"
-			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode())
-			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args")
-			done()
-		})
+			inputComponent.getInputDOMNode().value = "asdfasdfasdf100asdfasdfasdf";
+			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode());
+			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args");
+			done();
+		});
 		it("putting in 'a1s0d0f' results in changeQuantity getting called with '100'", function(done) {
-			inputComponent.getInputDOMNode().value = "a1s0d0f"
-			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode())
-			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args")
-			done()
-		})
+			inputComponent.getInputDOMNode().value = "a1s0d0f";
+			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode());
+			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args");
+			done();
+		});
 		it("putting in '-100' results in changeQuantity getting called with '100'", function(done) {
-			inputComponent.getInputDOMNode().value = "-100"
-			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode())
-			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args")
-			done()
-		})
+			inputComponent.getInputDOMNode().value = "-100";
+			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode());
+			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args");
+			done();
+		});
 		it("putting in '	    100	    ' results in changeQuantity getting called with '100'", function(done) {
-			inputComponent.getInputDOMNode().value = "	    100	    "
-			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode())
-			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args")
-			done()
-		})
+			inputComponent.getInputDOMNode().value = "	    100	    ";
+			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode());
+			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args");
+			done();
+		});
 		it("putting in ' 1 0 0 ' results in changeQuantity getting called with '100'", function(done) {
-			inputComponent.getInputDOMNode().value = " 1 0 0 "
-			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode())
-			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args")
-			done()
-		})
+			inputComponent.getInputDOMNode().value = " 1 0 0 ";
+			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode());
+			assert(changeQuantitySpy.calledWith(100), "changeQuantity not called with correct args");
+			done();
+		});
 		it("putting in 'asdfasdfasdf' results in changeQuantity getting called with '0'", function(done) {
-			inputComponent.getInputDOMNode().value = "asdfasdfasdf"
-			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode())
-			assert(changeQuantitySpy.calledWith(0), "changeQuantity not called with correct args")
-			done()
-		})
+			inputComponent.getInputDOMNode().value = "asdfasdfasdf";
+			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode());
+			assert(changeQuantitySpy.calledWith(0), "changeQuantity not called with correct args");
+			done();
+		});
 		it("putting in '' results in changeQuantity getting called with '0'", function(done) {
-			inputComponent.getInputDOMNode().value = ""
-			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode())
-			assert(changeQuantitySpy.calledWith(0), "changeQuantity not called with correct args")
-			done()
-		})
+			inputComponent.getInputDOMNode().value = "";
+			ReactTestUtils.Simulate.change(inputComponent.getInputDOMNode());
+			assert(changeQuantitySpy.calledWith(0), "changeQuantity not called with correct args");
+			done();
+		});
 
-	})
-})
+	});
+});
