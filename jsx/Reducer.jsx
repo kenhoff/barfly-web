1219a8aca10bd74b9b6a9dec7b903120e46ad478
Ken Hoff <thekenhoff@gmail.com>
2016-05-11 13:31:49 -0600

    redux-ing product card names

--- jsx/Reducer.jsx
+++ jsx/Reducer.jsx
@@ -1,9 +1,22 @@
 module.exports = function(state = {}, action) {
 	switch (action.type) {
 		case "UPDATE_PHONE":
-			return (Object.assign({}, state, {phone: action.phone, phoneDraft: action.phone}));
+			return (Object.assign({}, state, {
+				phone: action.phone,
+				phoneDraft: action.phone
+			}));
 		case "UPDATE_PHONE_DRAFT":
 			return (Object.assign({}, state, {phoneDraft: action.phoneDraft}));
+		case "UPDATE_PRODUCT":
+			if ("products" in state) {
+				var allProducts = state.products;
+			} else {
+				allProducts = {};
+			}
+			var newProducts = Object.assign({}, allProducts, {
+				[action.product.id]: action.product
+			});
+			return (Object.assign({}, state, {products: newProducts}));
 	}
 	return state;
 };
