fb083b0357f0a501c5ddb02b3ed41cdad605c6a4
Ken Hoff <thekenhoff@gmail.com>
2016-05-29 13:46:14 -0600

    RepName component

---
+++ jsx/Order/RepName.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var connect = require('react-redux').connect;
+
+var bartender = require('../Bartender.jsx');
+
+var RepName = React.createClass({
+	propTypes: {
+		repID: React.PropTypes.number.isRequired,
+		repName: React.PropTypes.string
+	},
+	render: function() {
+		return (
+			<span>{this.props.repName}</span>
+		);
+	}
+});
+
+var mapStateToProps = function(state, ownProps) {
+	let props = {};
+	if (("reps" in state) && (ownProps.repID in state.reps)) {
+		props.repName = state.reps[ownProps.repID].repName;
+	} else {
+		bartender.resolve({collection: "reps", id: ownProps.repID});
+	}
+	return props;
+};
+
+var ConnectedRepName = connect(mapStateToProps)(RepName);
+
+module.exports = ConnectedRepName;
