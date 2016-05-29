953b20b0bbb35592c464a679cab9e7f1655e4792
Ken Hoff <thekenhoff@gmail.com>
2016-05-29 12:52:23 -0600

    linting

--- jsx/Order/QuantityInputWithSize.jsx
+++ jsx/Order/QuantityInputWithSize.jsx
@@ -1,28 +1,28 @@
-var React = require('react')
-$ = require('jquery')
-var Button = require("react-bootstrap").Button
-var Glyphicon = require("react-bootstrap").Glyphicon
-var Input = require("react-bootstrap").Input
-var FormControls = require("react-bootstrap").FormControls
-var Row = require("react-bootstrap").Row
-var Col = require("react-bootstrap").Col
-var ListGroupItem = require('react-bootstrap').ListGroupItem
-var $ = require('jquery')
+var React = require('react');
+$ = require('jquery');
+var Button = require("react-bootstrap").Button;
+var Glyphicon = require("react-bootstrap").Glyphicon;
+var Input = require("react-bootstrap").Input;
+var FormControls = require("react-bootstrap").FormControls;
+var Row = require("react-bootstrap").Row;
+var Col = require("react-bootstrap").Col;
+var ListGroupItem = require('react-bootstrap').ListGroupItem;
+var $ = require('jquery');
 
 var QuantityInputWithSize = React.createClass({
 	getInitialState: function() {
-		return ({sizeName: "", quantity: this.props.quantity, containerName: "", packagingName: ""})
+		return ({sizeName: "", quantity: this.props.quantity, containerName: "", packagingName: ""});
 	},
 	render: function() {
 		if (this.props.inStarredProductsList && !this.props.starred) {
-			return (<div/>)
+			return (<div/>);
 		} else if (this.props.inOrderList && (!this.props.quantity || (this.props.quantity <= 0))) {
-			return (<div/>)
+			return (<div/>);
 		} else if (this.props.disabled) {
-			return (<FormControls.Static label={this.state.containerName + ", " + this.state.packagingName} placeholder="0" type="number" value={this.state.quantity}/>)
+			return (<FormControls.Static label={this.state.containerName + ", " + this.state.packagingName} placeholder="0" type="number" value={this.state.quantity}/>);
 		} else {
-			var minusButton = <Button onClick={this.decrement}>-</Button>
-			var plusButton = <Button onClick={this.increment}>+</Button>
+			var minusButton = <Button onClick={this.decrement}>-</Button>;
+			var plusButton = <Button onClick={this.increment}>+</Button>;
 
 			return (
 				<ListGroupItem>
@@ -40,23 +40,23 @@ var QuantityInputWithSize = React.createClass({
 						</Col>
 					</Row>
 				</ListGroupItem>
-			)
+			);
 		}
 	},
 	componentDidMount: function() {
 		// resolve size (container & packaging)
 		this.getContainerAndPackagingID(function(err, size) {
 			this.getContainerName(size.containerID, function(err, containerName) {
-				this.setState({containerName: containerName})
-			}.bind(this))
+				this.setState({containerName: containerName});
+			}.bind(this));
 			this.getPackagingName(size.packagingID, function(err, packagingName) {
-				this.setState({packagingName: packagingName})
-			}.bind(this))
-		}.bind(this))
+				this.setState({packagingName: packagingName});
+			}.bind(this));
+		}.bind(this));
 
 		// change negative or zero quantities to null
 		if (this.props.quantity <= 0) {
-			this.setState({quantity: null})
+			this.setState({quantity: null});
 		}
 	},
 	getContainerAndPackagingID: function(cb) {
@@ -64,53 +64,53 @@ var QuantityInputWithSize = React.createClass({
 			url: process.env.BURLOCK_API_URL + "/sizes/" + this.props.sizeID,
 			method: "GET",
 			success: function(size) {
-				cb(null, size)
+				cb(null, size);
 			}.bind(this)
-		})
+		});
 	},
 	getContainerName: function(containerID, cb) {
 		$.ajax({
 			url: process.env.BURLOCK_API_URL + "/containers/" + containerID,
 			method: "GET",
 			success: function(container) {
-				cb(null, container.containerName)
+				cb(null, container.containerName);
 			}
-		})
+		});
 	},
 	getPackagingName: function(packagingID, cb) {
 		$.ajax({
 			url: process.env.BURLOCK_API_URL + "/packaging/" + packagingID,
 			method: "GET",
 			success: function(packaging) {
-				cb(null, packaging.packagingName)
+				cb(null, packaging.packagingName);
 			}
-		})
+		});
 	},
 	componentWillReceiveProps: function(nextProps) {
 		// change negative or zero quantities to null
 		if (nextProps.quantity <= 0) {
-			this.setState({quantity: null})
+			this.setState({quantity: null});
 		} else {
-			this.setState({quantity: nextProps.quantity})
+			this.setState({quantity: nextProps.quantity});
 		}
 
 	},
 	handleInputChange: function(event) {
-		this.changeQuantity(event.target.value.replace(/[^0-9]/g, "").trim())
+		this.changeQuantity(event.target.value.replace(/[^0-9]/g, "").trim());
 	},
 	changeQuantity: function(quantity) {
 		// clean up event.target.value - make it into a number
 		if (!quantity || quantity == "") {
-			this.props.changeQuantity(0)
+			this.props.changeQuantity(0);
 		} else {
-			this.props.changeQuantity(parseInt(quantity))
+			this.props.changeQuantity(parseInt(quantity));
 		}
 	},
 	increment: function() {
 		if (!this.state.quantity || (this.state.quantity == 0)) {
-			this.changeQuantity(1)
+			this.changeQuantity(1);
 		} else {
-			this.changeQuantity(parseInt(this.state.quantity) + 1)
+			this.changeQuantity(parseInt(this.state.quantity) + 1);
 		}
 	},
 	decrement: function() {
@@ -118,17 +118,17 @@ var QuantityInputWithSize = React.createClass({
 			// this.changeQuantity(1)
 			// do nothing
 		} else if (this.state.quantity == 1) {
-			this.changeQuantity(0)
+			this.changeQuantity(0);
 		} else {
-			this.changeQuantity(parseInt(this.state.quantity) - 1)
+			this.changeQuantity(parseInt(this.state.quantity) - 1);
 		}
 	},
 	changeStarred: function() {
 		this.props.changeStarred({
 			newStarredValue: !this.props.starred,
 			sizeID: this.props.sizeID
-		})
+		});
 	}
-})
+});
 
-module.exports = QuantityInputWithSize
+module.exports = QuantityInputWithSize;
